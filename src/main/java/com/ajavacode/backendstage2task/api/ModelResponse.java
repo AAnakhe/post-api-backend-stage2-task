275e3dda7b8b2f8d49ecc5c1d6792aa8ec0d3810
Anakhe Ajayi <id3velope@gmail.com>
2022-11-06 15:58:30 +0100

    Update ModelResponse.java

--- src/main/java/com/ajavacode/backendstage2task/api/ModelResponse.java
+++ src/main/java/com/ajavacode/backendstage2task/api/ModelResponse.java
@@ -7,13 +7,14 @@ import lombok.Setter;
 @Setter
 public class ModelResponse {
 
+    private String slackUsername;
+    private Operator operation_type;
+    private Integer result;
+
     public ModelResponse(String slackUsername, Operator operation_type,  Integer result) {
         this.slackUsername = slackUsername;
         this.operation_type = operation_type;
         this.result = result;
     }
 
-    private String slackUsername;
-    private Operator operation_type;
-    private Integer result;
 }
